a2008044c61020777f0985c7772fa59ef8bf57a2
bansalsparsh <115392431+bansalsparsh@users.noreply.github.com>
2022-12-04 13:07:05 +0530

    Create exer1.py

---
+++ exer1.py
@@ -0,0 +1,12 @@
+def findMaxOfThreeNumbers(a,b,c):
+  if (num1 > num2 and num1 > num3):
+    print("Greatest number:",num1)
+  elif (num2 > num1 and num2 > num3):
+    print("Greatest number:",num2)
+  elif (num3 > num1 and num3 > num 2):
+    print("Greatest number:",num3)
+   
+return findMaxOfThreeNumbers
+num1=int(input("a: "))
+num2=int(input("b: "))
+num3=int(input("c: "))
